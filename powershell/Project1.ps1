9979d1fe20f3ea946d26faaae521100af0caa230
ethanputt <111774249+ethanputt@users.noreply.github.com>
2022-09-13 09:07:33 -0400

    Add files via upload

---
+++ powershell/Project1.ps1
@@ -0,0 +1,14 @@
+﻿#Variables:
+
+$Name = 'Ethan'
+$Time = (get-date).ToString('T')
+$TotalHour = (Get-Date).Hour
+
+
+#Script:
+
+If ($TotalHour -lt 12) {"Good morning $Name! It is $Time. Have a great rest of your day!"}
+
+ElseIf ($Hour -gt 17) {"Good Evening $Name! It is $Time. You should eat dinner and then get ready for bed."}
+
+Else {"Good Afternoon $Name! It is $Time. It is not to late to go to the gym. Get moving!"}
