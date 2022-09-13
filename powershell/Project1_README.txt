83cabaf521648578d29437e6ad96754806af87d0
ethanputt <putter@mail.uc.edu>
2022-09-13 10:44:38 -0400

    Initial commit

---
+++ powershell/Project1_README.txt
@@ -0,0 +1,10 @@
+Hello!
+
+I have designed this script to tell you the time and provide a greeting based on the time of day it is. The output is designed to greet you with your name, the time, and then an encouraging message dependent on the time of day.
+
+Please edit the name variable to the User's name in order for it to be custom. I chose not to use the system name so that it would feel more personable.
+
+To edit the name variable please located the variable $Name under the varibale section and replace 'Ethan' with the new User's name.
+
+If you have any questions please let me know!
+
--- powershell/sysinfo.ps1
+++ powershell/sysinfo.ps1
@@ -1,10 +1,14 @@
-﻿function getIP{
-    (get-netipaddress).ipv4address | Select-String "10*"
-}
-write-host(getIP)
+﻿
+$day = (get-date).day
+$dayofweek = (get-date).dayofweek
+$month = (Get-Culture).DateTimeFormat.GetMonthName(9)
+$year = (get-date).year
 
-$IP = getIP
+$Version= $Host.Version.Major
+$Computer = $env:COMPUTERNAME
+$Name = $env:USERNAME
+$IP = (Get-NetIPAddress -AddressFamily IPv4 -InterfaceAlias Ethernet).IPAddress
 
-Write-Host("This machine's IP is $IP")
+$Body = "This machine's IP is $IP. User is $Name. Hostname is $Computer. Powershell Version $Version. Today's Date is $dayofweek, $month $day $year. This is Ethan Putt's submission for Lab 3. Thank you!"
 
-Write-Host("This machine's IP is {0}" -f $IP)
+Send-MailMessage -To "putter@mail.uc.edu", "leonardf@ucmail.uc.edu" -From "emanp18@gmail.com" -Subject "IT3038C Windows SysInfo" -Body $Body -SmtpServer smtp.gmail.com -port 587 -UseSSL -Credential (Get-Credential)
